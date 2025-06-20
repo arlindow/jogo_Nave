561f4c026df6a0c9f347006e4562c767ec3d16e5
arlindo wilson <arlindowilsonaws@gmail.com>
2025-06-19 23:54:18 -0300

    desenhar um retangulo

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
-function minhaFuncao(){
-    alert ("teste");
+var canvas = document.getElementById('meu_canvas');
+var context = canvas.getContext('2d');
 
-}
+// Desenhar um retângulo
+context.fillRect(50, 50, 100, 100);
+// Se trocarmos fillRect por strokeRect, veremos apenas o contorno:
+// context.strokeRect(50, 50, 100, 100);
