c3f37ffca81f7744e783e9622806a229a7a4b57b
arlindo wilson <arlindowilsonaws@gmail.com>
2025-06-20 22:17:35 -0300

    retangulo 2

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
 var canvas = document.getElementById('meu_canvas');
 var context = canvas.getContext('2d');
 
-// Desenhar um retângulo
+// Preenchimento vermelho
+context.fillStyle = 'red';
 context.fillRect(50, 50, 100, 100);
-// Se trocarmos fillRect por strokeRect, veremos apenas o contorno:
-// context.strokeRect(50, 50, 100, 100);
+// Contorno azul, com espessura de 3px
+context.lineWidth = 3;
+context.strokeStyle = 'blue';
+context.strokeRect(50, 50, 100, 100);
+
