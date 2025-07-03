7ce701cde7705b91c2d9e9ef89a19529eedfa192
arlindo wilson <arlindowilsonaws@gmail.com>
2025-07-02 22:18:29 -0300

    recorte clipping

--- script.js
+++ script.js
@@ -4,15 +4,13 @@ var context = canvas.getContext('2d');
 
 // carregar a imagem
 var imagem = new Image();
-imagem.src = 'img/ovni.png';
+imagem.src = 'img/explosao.png';
 imagem.onload = function () {
-    // começar na posição 20
-    var x = 20;
-    // desenhar as imagens
-    for (var i = 1; i <= 5; i++){
-        context.drawImage(imagem, x, 20, 64, 32);
-        x += 70;
-        // Ampliando para o dobro do tamanho
-        //context.drawImage(imagem, x, 20, 128, 64);
+    context.drawImage(
+    imagem,
+    80, 10, 60, 65, // Área de recorte (clipping)
+    20, 20, 60, 65 // Desenho no Canvas
+    );
+        
     }
-}
+
