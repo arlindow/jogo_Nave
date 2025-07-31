478f4e8fa61a8b490ce18753e8b7a6ad930d288f
arlindo wilson <arlindowilsonaws@gmail.com>
2025-07-30 22:23:00 -0300

    Controlando o tempo da animação

--- script.js
+++ script.js
@@ -7,11 +7,21 @@ var x = 20;
 var y = 100;
 var raio = 5;
 
+// Controlando o tempo da animação
+// Momento inicial
+var anterior = new Date().getTime();
+
 // Iniciar a animação
 requestAnimationFrame(mexerBola);
 
 // Função de animação
 function mexerBola() {
+    // Momento atual
+    var agora = new Date().getTime();
+
+    // Diferença
+    var decorrido = agora - anterior;
+
     // Aqui uma bolinha se deslocará
     // Limpar o Canvas
     context.clearRect(0, 0, canvas.width, canvas.height);
@@ -19,9 +29,14 @@ function mexerBola() {
     context.beginPath();
     context.arc(x, y, raio, 0, Math.PI*2);
     context.fill();
-    // Deslocar 20 pixels para a direita
-    x += 20;
-    // Chamar o próximo ciclo da animação
+
+    // Guardamos o instante para o próximo ciclo
+    anterior = agora;
     requestAnimationFrame(mexerBola);
+
+    // Deslocar 20 pixels por segundo
+    var velocidade = 20;
+    x += velocidade * decorrido / 1000;
+    
     
 }
